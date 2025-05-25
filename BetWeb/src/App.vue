16d5bb8a05c2cbebd2c249ebacf641b96b8fa064
AlexMartin <alex.martin.7e7@itb.cat>
2025-05-25 16:14:09 +0200

    commit base

---
+++ BetWeb/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navigation />
+    <main class="container">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Navigation from './components/Navigation.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<style src="./assets/style.css"></style>
---
+++ BetWeb/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// Importa tus vistas
+import Home from '../views/Home.vue'
+import Links from '../views/Links.vue'
+import Contact from '../views/Contact.vue'
+
+const routes = [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/links', name: 'Links', component: Links },
+    { path: '/contact', name: 'Contact', component: Contact }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
