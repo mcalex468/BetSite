7ff58397225938a46a227b28a5905c2e4adcf3cd
AlexMartin <alex.martin.7e7@itb.cat>
2025-06-04 00:37:00 +0200

    commit backend

--- BetWebBack/server.js
+++ BetWebBack/server.js
@@ -12,11 +12,6 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
-// EDICIÓN
-const userRoutes = require('./routes/user')
-app.use('/api/user', userRoutes)
-
-
 app.listen(PORT, () => {
     console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
